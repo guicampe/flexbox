7a9c5f0fdb9e75327d7f876c4b9ec077f00aa323
Guilherme Ramos Campestrini <gramoscampestrini@gmail.com>
2025-05-07 16:44:01 -0300

    conteudo adicionado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function mudarFlex(flexFlow, width, height, margin, itemWidth) {
     const container = document.querySelector('.container1');
-    const caixas = [document.querySelector('#cx1'), document.querySelector('#cx2'), document.querySelector('#cx3')];
+    const caixas = [document.querySelector('#cx11'), document.querySelector('#cx12'), document.querySelector('#cx13')];
 
     container.style.flexFlow = flexFlow;
     container.style.width = width;
@@ -25,4 +25,24 @@ function flexColumn() {
 
 function flexColumnreverse() {
     mudarFlex('column-reverse nowrap', '50px', '200px', '10px 0px', '30px');
+}
+
+function mudarWrap(flexFlow, width, height) {
+    const container2 = document.querySelector('.container2');
+
+    container2.style.flexFlow = flexFlow;
+    container2.style.width = width;
+    container2.style.height = height;
+} 
+
+function noWrap() {
+    mudarWrap('row nowrap', '100px', '50px');
+}
+
+function wrap() {
+    mudarWrap('row wrap', '100px', '100px');
+}
+
+function resetar() {
+    mudarWrap('row nowrap', '200px', '50px');
 }
